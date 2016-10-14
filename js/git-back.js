89cfb45f4d1f241d36f8f8799d2c5a96d9818714
Andres Garcia <andresgarciar96@gmail.com>
2016-10-14 16:56:08 -0400

    add back end logic and .env file

--- js/git-back.js
+++ js/git-back.js
@@ -1,8 +1,26 @@
 var apiKey = require('./../.env').apiKey;
 
-Weather = function(){
+function Repositories(){
 }
 
-Repositories = function()
-{
-}
+Repositories.prototype.getRepositories = function(username){
+  $.get('https://api.github.com/users/' + username + '?access_token=' + apiKey).then(function(response){
+    console.log(response);
+    $(".user").text(response.login);
+    }).fail(function(error){
+    console.log(error.responseJSON.message);
+    $(".user").text("User not Found");
+  });
+
+  $.get('https://api.github.com/users/' + username + '/repos?access_token=' + apiKey).then(function(response){
+    console.log(response);
+    $(".userRepos").text("");
+    for(var i=0; i<response.length;i++){
+      $(".userRepos").append("<li class='userRepo'>" + response[i].name + "<ul><li class='dateOfRepo'>" + response[i].created_at +  "<li class='description'>" + response[i].description + "</li></ul>" + "</li>")
+    }
+  }).fail(function(error){
+    console.log(error.responseJSON.message);
+  });
+};
+
+exports.repoModule = Repositories;
--- js/git-front.js
+++ js/git-front.js
@@ -1,8 +1,8 @@
-var Repos = require('./../js/Repos.js').repoModule;
+var Repositories = require('./../js/Repos.js').repoModule;
 
 $(document).ready(function(){
-  var cRepo = new Repositories;
-  $('#getRepos').click(function(){
+  var Repo = new Repositories;
+  $('#butt').click(function(){
     var userName = $('#name').val();
     Repo.getRepositories(userName);
   });
